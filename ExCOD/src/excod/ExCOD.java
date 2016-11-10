e78eb45f0cc1e345eed42376b99e57166fc0d460
Adrián Silva <adri4silva@gmail.com>
2016-11-10 09:25:52 +0100

    Visualizacion por pantalla

--- ExCOD/src/excod/ExCOD.java
+++ ExCOD/src/excod/ExCOD.java
@@ -15,6 +15,7 @@ public class ExCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Commit A");
         
     }
     
